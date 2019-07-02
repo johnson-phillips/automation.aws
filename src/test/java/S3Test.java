a13b4803177ac3aaddf02af5966eb49c2297d526
johnson_phillips <johnson.phillips@mheducation.com>
2019-07-02 12:18:38 -0400

    added sample test

---
+++ src/test/java/S3Test.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.UUID;
+
+public class S3Test {
+
+    @Test
+    public void unitTest() throws Exception
+    {
+//        System.out.print("\r\n" + "Test started");
+//        String folder = UUID.randomUUID().toString();
+//        String filename = UUID.randomUUID().toString() + ".txt";
+//        String content = UUID.randomUUID().toString();
+//        S3.createFolder(folder);
+//        S3.uploadFile(folder + "/" + filename,content);
+//        String text = S3.getFile(folder+"/"+filename);
+//        Assert.assertEquals(text,content);
+//        System.out.print("\r\n" + "Test end");
+//        S3.deleteFile(folder + "/" + filename);
+//        S3.deleteFolder(folder);
+//        InputStream stream = S3.getStream("print/items/aed71dce8d74cbde0e357eeb98acc912.png");
+//        File file = new File(UUID.randomUUID().toString()+".png");
+//        FileUtils.copyInputStreamToFile(stream,file);
+//        Assert.assertEquals(file.exists(),true);
+//        file.delete();
+    }
+}
